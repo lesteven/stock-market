9d09b64215b9a2b61dd056b19ac6c9eded6030d9
steven <stevenle2011@gmail.com>
2017-06-29 12:32:15 +0700

    initial commit

--- server.js
+++ server.js
@@ -13,10 +13,6 @@ app.get('*.js', function (req, res, next) {
 app.use(express.static(__dirname + '/dist'));
 app.use('/',express.static(__dirname + '/public'));
 
-app.use('/goodbye',function(req,res){
-	res.send('goodbye world!')
-})
-
 
 app.listen(port,function(){
 	console.log(`Listening on port ${port}`)
